79939ecb73decc1948aadb151b9890635fe987f3
TatianaSteshakova <65027776+TatianaSteshakova@users.noreply.github.com>
2023-03-12 22:17:40 +0300

    Add files via upload

---
+++ Homework5.5.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(0, "Rus");
+
+    int n, m;
+    cout << "Введите количество строк и столбцов двумерного массива (через пробел): " << endl;
+    cin >> n >> m;
+
+    int** mas = new int* [n];
+    for (int i = 0; i < n; i++)
+    {
+        mas[i] = new int[m];
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cout << "Введите число mas[" << i << "][" << j << "]: ";
+            cin >> mas[i][j];
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        int temp;
+        for (int j = i; j < m; j++)
+        {
+            temp = mas[i][j];
+            mas[i][j] = mas[j][i];
+            mas[j][i] = temp;
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            printf("%5i", mas[i][j]);
+        }
+        cout << endl;
+    }
+}
